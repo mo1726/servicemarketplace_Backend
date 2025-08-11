a9b02e0ec44825d1842b0fcf24daac13ed021d97
mo1726 <mlahrech123@gmail.com>
2025-08-11 23:11:02 +0100

    update config

--- src/main/java/com/example/service_marketplace/config/SecurityConfig.java
+++ src/main/java/com/example/service_marketplace/config/SecurityConfig.java
@@ -89,7 +89,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://lightslategray-cobra-122495.hostingersite.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // ✅ include OPTIONS
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true); // ✅ for token auth
